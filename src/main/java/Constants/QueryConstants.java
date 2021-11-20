20f0cc2c69c6eb724c10f2df7a16b0648738a68c
Shiv Gaurang Desai <sh721068@dal.ca>
2021-11-20 17:20:00 -0400

    feat: added query operations for CreateDb, UseDb, CreateTable, DropTable

---
+++ src/main/java/Constants/QueryConstants.java
@@ -0,0 +1,11 @@
+package Constants;
+
+public class QueryConstants {
+
+    public static final String CREATE_DATABASE="CREATE DATABASE";
+    public static final String USE_DATABASE="USE DATABASE";
+    public static final String CREATE_TABLE="CREATE TABLE";
+    public static final String SELECT="SELECT";
+    public static String CURRENT_DB="";
+
+}
